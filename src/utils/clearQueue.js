b06235154db1d9c51245d1eb9bd93e10d7c76e21
SlotyHolly <matiasmusso3032@gmail.com>
2023-11-20 22:37:02 -0300

    Creacion ClearQueue.js

---
+++ src/utils/clearQueue.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
+function clearJsonJsonFile(filePath) {
+    try {
+        // Escribe un objeto vacío o la estructura inicial deseada
+        fs.writeFileSync(filePath, JSON.stringify({}, null, 2), 'utf8');
+        console.log(`El archivo JSON (${filePath}) ha sido limpiado.`);
+    } catch (error) {
+        console.error(`Error al limpiar el archivo JSON (${filePath}):`, error);
+    }
+}
+
+// Ruta del archivo JSON (ajusta esto a tu archivo específico)
+const pathJSON = path.join(__dirname, '../../queue.json');
+
+// Llamar a la función para limpiar el archivo JSON
+clearJsonJsonFile(pathJSON);
