66f3a20df659d9391f0397d3af0f43b631335607
SlotyHolly <matiasmusso3032@gmail.com>
2023-12-02 00:09:36 -0300

    'l;';'kkl;jih

--- src/utils/firstJSON.js
+++ src/utils/firstJSON.js
@@ -6,27 +6,21 @@ const DATA_FILE = path.join(__dirname, '../../queue.json');
 
 function firstJSON() {
     try {
-        // Leer el archivo JSON
         const jsonData = fs.readFileSync(DATA_FILE, 'utf8');
-
-        // Convertir los datos a un array de JavaScript
         let data = JSON.parse(jsonData);
 
-        // Verificar si hay elementos en el array
+        // Verificar si data es un array
+        if (!Array.isArray(data)) {
+            console.error('El contenido del archivo JSON no es un array');
+            return null;
+        }
+
         if (data.length === 0) {
             return null;
         }
 
-        // Obtener el primer elemento
-        const firstElement = data[0];
-
-        // Eliminar el primer elemento del array
-        data.shift();
-
-        // Guardar el array actualizado en el archivo JSON
+        const firstElement = data.shift();
         fs.writeFileSync(DATA_FILE, JSON.stringify(data, null, 2));
-
-        // Devolver el primer elemento
         return firstElement;
     } catch (error) {
         console.error('Error al leer o actualizar el archivo JSON:', error);
